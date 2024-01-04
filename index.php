852bf71292ab02f20e9fe85987b7d7451f54dc84
malikazis12 <147013580+malikazis12@users.noreply.github.com>
2024-01-04 11:33:57 +0700

    penggantian nama

--- index.php
+++ index.php
@@ -32,12 +32,12 @@ include_once("cek_login.php");
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
+            <h1>Rawat Inap Darurat</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
+              <li class="breadcrumb-item active">Rawat Inap</li>
             </ol>
           </div>
         </div>
