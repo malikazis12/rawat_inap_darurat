d3522951221b46997efac574ed355a64a207b95b
malikazis12 <147013580+malikazis12@users.noreply.github.com>
2024-01-18 09:22:31 +0700

    Penambahan form baru di pendaftran

--- sidebar.php
+++ sidebar.php
@@ -37,7 +37,7 @@
                with font-awesome or any other icon font library -->
 
           <li class="nav-item">
-            <a href="../widgets.html" class="nav-link">
+            <a href="pendaftran.php" class="nav-link">
               <i class="nav-icon fas fa-id-card"></i>
               <p>
                 Pendaftaran
